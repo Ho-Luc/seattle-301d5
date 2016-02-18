dd59bfa7d8da20bd90df6217c27d0799b40afa82
Joseph McClenahan <jmcclena94@gmail.com>
2016-02-18 13:23:25 -0800

    added comments to describe fns & execution paths in articleView.js

--- class-14-managing-state/luc-joe/scripts/articleView.js
+++ class-14-managing-state/luc-joe/scripts/articleView.js
@@ -13,6 +13,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /**articleView.populateFilters is called in the articleView.index function which is called whenever we change to a new page state.  This function uses handlebars.js to populate the author and category filters using the #option-template compiler.  For the article filter, it calls Article.allAuthors to get a list of all unique authors, then for each author, compiles a handlebar template, then, preventing duplicates, appends the filter entry to the filter.  For the column filter, we call Article.allCategories and use a SQL query to find distinct categories then compile the handlebar template for categories and append those category entries to the category list. **/
   articleView.populateFilters = function() {
     var options,
       template = Handlebars.compile($('#option-template').text());
@@ -38,6 +39,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /** articleView.handleFilters is called in the articleView.index function.  When you change a filter you change the url path to add a /author/ or /category/ depending on what filter you select.  The .replace method removes the "-filter" from the end of either author-filter or category-filter.  Second, this function adds a path to the specific author or category, and removes whitespace and replaces it with a + with the regex call in the .replace method. **/
   articleView.handleFilters = function() {
     $('#filters').one('change', 'select', function() {
       resource = this.id.replace('-filter', '');
@@ -118,6 +120,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /** articleView.index is called every time we change page state.  It is called in articleController.index.  articleView.index first hides all the other sections, in this case the "about" section and removes all articles in the articles section.  For each article passed into the articleView.index function we call render() to append it to the page.  Then we populate the filters with articleView.populateFilters, then call the articleView.handleFilters functions to handle when we switch filter values.  Finally, if there are more than one articles, we only display the first two elements of each article. **/
   articleView.index = function(articles) {
     $('#articles').show().siblings().hide();
 
@@ -128,6 +131,7 @@
 
     articleView.populateFilters();
     // COMMENT: What does this method do?  What is it's execution path?
+    /** We call this here because every time we change page state, we need to know what to display for the URL.  This functions picks up on what filter we change to and alters the url path, as described above. **/
     articleView.handleFilters();
 
     // DONE: Replace setTeasers with just the truncation logic, if needed:
